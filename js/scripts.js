a0a5257463e7f985e2a878c243d200f1d81c4b18
Kim Dang & Chris LeCompte <student@epicodus.com>
2015-05-26 16:48:53 -0700

    Cryptosquare encrypts

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,55 @@ var cryptosquare = function(string) {
   var array = []
   var string_length = str.length;
   var dimension = Math.sqrt(string_length);
-  var length = Math.round(dimension);
-  array.push(length);
-  array.push(length + 1);
+  if (dimension % 1 === 0) {
+    array.push(dimension, dimension);
+  } else {
+    var length = Math.ceil(dimension);
+    if (((length * length) - string_length) > (length - 1)) {
+      array.push(length - 1, length)
+    } else {
+      array.push(length, length);
+    }
+  }
 
-  return array;
+  var msg_array = str.split("");
+  var loop_counter = 0;
+  var string_array = [];
+  msg_array.forEach(function(entry) {
+    if (loop_counter < array[1]) {
+      string_array.push(entry);
+      ++loop_counter;
+    } else {
+      string_array.push('\n');
+      string_array.push(entry);
+      loop_counter = 1;
+    }
+  });
+
+  new_array = string_array.join().replace(/,+/g, '').split("\n");
+  var encrypted_array = [];
+
+  for(var i = 0; i < (new_array.length) + 1; i += 1) {
+    for(var k = 0; k < array[0]; k += 1) {
+      encrypted_array.push(new_array[k][i]);
+
+
+
+    }
+  }
+  loop_counter = 0
+  brand_new_array = []
+  encrypted_array.forEach(function(entry) {
+    if (loop_counter < 5) {
+      brand_new_array.push(entry);
+      ++loop_counter;
+    } else {
+      brand_new_array.push(" ");
+      brand_new_array.push(entry);
+      loop_counter = 1;
+    }
+  });
+  return brand_new_array.join().replace(/,+/g, '')
+
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,12 +2,28 @@ describe('cryptosquare', function() {
   // it('takes in a string that is 9 characters long and determines the rectangle dimensions', function() {
   //   expect(cryptosquare('123456789')).to.eq([3, 3]);
   // });
+  //
+  // it('takes in a string that is 5 characters long and determines the rectangle dimensions', function() {
+  //   expect(cryptosquare('12345')).to.eq([2, 3]);
+  // });
+  //
+  // it('takes in a string that is 69 characters long and determines the rectangle dimensions', function() {
+  //   expect(cryptosquare('dont compare yourself to others, compare yourself to the person you were yesterday')).to.eq([8, 9]);
+  // });
+  //
+  // it('takes in a string that is 15 characters long and determines the rectangle dimensions', function() {
+  //   expect(cryptosquare('123456789123451')).to.eq([4, 4]);
+  // });
+  //
+  // it('takes in a string that is 12 characters long and determines the rectangle dimensions', function() {
+  //   expect(cryptosquare('123456789123')).to.eq([3, 4]);
+  // });
+  //
+  // it('takes in a string that is 13 characters long and determines the rectangle dimensions', function() {
+  //   expect(cryptosquare('1234567891234')).to.eq([4, 4]);
+  // });
 
-  it('takes in a string that is 5 characters long and determines the rectangle dimensions', function() {
-    expect(cryptosquare('12345')).to.eq([2, 3]);
-  });
-
-  it('takes in a string that is 5 characters long and determines the rectangle dimensions', function() {
-    expect(cryptosquare('dont compare yourself to others, compare yourself to the person you were yesterday')).to.eq([8, 9]);
+  it('prints the string as a rectangle', function() {
+    expect(cryptosquare('1234')).to.eq("12" + "\n" + "34");
   });
 });
